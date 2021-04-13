d9ab683c4b7776bbd20249bc160b009a52713d1d
Taiyahm <taiyahm23@gmail.com>
2021-04-13 13:51:15 -0400

    added pass code

--- src/App.js
+++ src/App.js
@@ -6,21 +6,81 @@ var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 var symbols = ["!","#","$","^","&","*","-","=","+","_","?",];
 
 function App() {
+
+  function generatePassword(){
+
+    // Prompts user to enters password length
+    var passwordLength = parseInt(prompt("Enter the length of your password between 8 and 128 characters:"));
+  
+    if (passwordLength < 8) {
+      alert('Must be at least 8 characters!');
+      return;
+    }
+    if (passwordLength > 128) {
+      alert('Must be less than 128 characters!');
+      return;
+    }
+  
+    // Prompts user to include uppercase,lowercase,numbers, and/or symbols
+    var includeUpper =  window.confirm("Would you like to include uppercase letters?");
+    var includeLower = window.confirm("Would you like to include lowercase letters?");
+    var includeNumbers = window.confirm("Would you like to include numbers?");
+    var includeSymbols = window.confirm("Would you like to include symbols?");
+  
+    // alerts user to select criteria if none is chosen
+    if (!includeNumbers && !includeUpper && !includeSymbols && !includeLower) {
+      alert('Must select at least one of the criteria!');
+      return;
+    }	
+  
+    var confirmedArr = [];
+    var passArr= [];
+  
+    if (includeNumbers){
+     confirmedArr = confirmedArr.concat(numbers);
+      
+    }
+    if (includeUpper){
+     confirmedArr = confirmedArr.concat(upper);
+    
+    }
+    if (includeLower){
+     confirmedArr = confirmedArr.concat(lower);
+    
+    }
+    if (includeSymbols){
+     confirmedArr = confirmedArr.concat(symbols);
+    }
+    console.log(confirmedArr)
+    for (var i = 0; i < passwordLength; i++) {
+      passArr.push (confirmedArr[Math.floor(Math.random() * confirmedArr.length)]); 
+    }
+    
+    return passArr.join("") ;
+  }
+  var password = generatePassword();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div class="wrapper">
+      <header>
+        <h1>Password Generator</h1>
       </header>
+      <div class="card">
+        <div class="card-header">
+          <h2>Generate a Password</h2>
+        </div>
+        <div class="card-body">
+          <textarea value={password} 
+            readonly
+            id="password"
+            placeholder="Your Secure Password"
+            aria-label="Generated Password"
+          ></textarea>
+        </div>
+        <div class="card-footer">
+          <button id="generate" class="btn" onClick={generatePassword} >Generate Password</button>
+        </div>
+      </div>
     </div>
   );
 }
